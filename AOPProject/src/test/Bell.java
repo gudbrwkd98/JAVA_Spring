a71ab4c6b64b98cff763228150d2bbd1db35ceef
DESKTOP-1R03JR5\gudrb <gudrbwkd98@gmail.com>
2020-12-31 08:41:20 +0900

    AOP 예제

---
+++ AOPProject/src/test/Bell.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Bell {
+	public void startBell(){
+		System.out.println("시작 종");
+	}
+	
+	public void endBell() {
+		System.out.println("끝 종");
+	}
+}
---
+++ AOPProject/src/test/Student.java
@@ -0,0 +1,30 @@
+package test;
+
+public class Student {
+
+	
+	public void study1() {
+	
+		System.out.println("영어");
+		
+	}
+	
+	public void study2() {
+		
+		System.out.println("국어");
+		
+	}
+	
+	
+	public void study3() {
+		
+		System.out.println("물리");
+		
+	}
+	
+	public void study4() {
+		
+		System.out.println("수학");
+	
+	}
+}
